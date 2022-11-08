c3fef0633106b9537a032fd14afb7a55df4b0f61
David Kaufmann <kaufmann.david@web.de>
2022-11-08 12:42:09 +0100

    Change to simpler solution for is prime

--- demos/Arithmetik4.hs
+++ demos/Arithmetik4.hs
@@ -1,7 +1,12 @@
 module Arithmetik4 where
-
 import Arithmetik3 (root)
+isPrime n
+    | n < 2     = error "Zu kleine Zahl fuer Primzahltest"
+    | otherwise = nHasNoDivisorGreaterThan 2
+  where 
+        upto = root 2 n
+        nHasNoDivisorGreaterThan k
+          | k > upto       = True
+          | n `mod` k == 0 = False
+          | otherwise      = nHasNoDivisorGreaterThan (k + 1)
 
-isPrime n = not (any (divides n) [2..root 2 n])
-  where
-    divides p q = p `mod` q == 0
